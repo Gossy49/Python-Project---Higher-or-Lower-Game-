2b73a0beaa7ac71e139a2a7a3ab914f3e1102fbb
God'stime Okolo <godstimeokolo25@gmail.com>
2025-12-10 16:55:45 +0100

    Game 95% complete, add logic to make compare B become A if compare B is greater than A

--- Higher_lower.py
+++ Higher_lower.py
@@ -6,39 +6,73 @@ import random
 #keep user score
 current_score = 0
 
+game = True
+
 
 #print B
 game_art = print (logo)
 
-
-
-#get the name, description and country
+        #get the name, description and country
 
 a_choice = random.choice (data)
 a_followers = a_choice ['follower_count']
-a_ccompare = print(f'Compare A:  {a_choice['name']}, {a_choice['description']}, {a_choice['country']} ')
+a_compare = (f'Compare A:  {a_choice['name']}, {a_choice['description']}, {a_choice['country']} ')
+print (a_compare)
 
 game_art2 = print (vs)
 
 
 b_choice = random.choice (data)
 b_followers = b_choice['follower_count']
-b_compare = print(f'Compare B:  {b_choice['name']}, {b_choice['description']}, {b_choice['country']} ')
+b_compare = f'Compare B:  {b_choice['name']}, {b_choice['description']}, {b_choice['country']} '
+print(b_compare)
 
+
+
+
+while game : 
 #get user's answer
-user_answer = input ("who has more followers? Type 'A' or 'B':  ")
 
-if user_answer == 'A' and a_followers > b_followers:
-    current_score += 1
-    print (f"You're correct! Current score: {current_score}.")
-
-elif user_answer == 'B' and b_followers > a_followers:
-    a_ccompare = b_compare
-    current_score +=1
-    print (f"You're correct! Current score: {current_score}.")
     
-else:
-    print(f"Sorry, that's wrong. Final score {current_score}")
+    user_answer = input ("who has more followers? Type 'A' or 'B':  ")
+
+    if user_answer == 'A' and a_followers > b_followers:
+
+        current_score += 1
+        print (f"You're correct! Current score: {current_score}.")
+        print(a_compare)
+
+        game_art2 = print (vs)
+
+        b_choice = random.choice (data)
+        b_followers = b_choice['follower_count']
+        b_compare = f'Compare B:  {b_choice['name']}, {b_choice['description']}, {b_choice['country']} '
+        print(b_compare)
+
+        
+
+
+
+    elif user_answer == 'B' and b_followers > a_followers:
+        current_score +=1
+        print (f"You're correct! Current score: {current_score}.")
+        a_choice = b_choice
+        a_compare = (f'Compare A:  {a_choice['name']}, {a_choice['description']}, {a_choice['country']} ')
+
+        print (a_compare)
+
+        game_art2 = print (vs)
+
+        b_choice = random.choice (data)
+        b_followers = b_choice['follower_count']
+        b_compare = f'Compare B:  {b_choice['name']}, {b_choice['description']}, {b_choice['country']} '
+        print(b_compare)
+
+
+        
+    else:
+        print(f"Sorry, that's wrong. Final score {current_score}")
+        game = False
 
 
         
